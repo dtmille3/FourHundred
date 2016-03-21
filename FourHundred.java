296394e03740dd87b2f7b923759d5300365444d9
Dasnyl Miller <dasnyl1272007@gmail.com>
2016-03-21 02:05:14 -0700

    FourHundredJavaProgram

---
+++ FourHundred.java
@@ -0,0 +1,11 @@
+package com.java24hours;
+
+class FourHundred {
+    public static void main(String[] arguments){
+        for (int dasnyl = 0; dasnyl < 400; dasnyl++){
+            if (dasnyl % 13 == 0){
+                System.out.println("#: " + dasnyl);
+            }
+        }
+    }
+}
